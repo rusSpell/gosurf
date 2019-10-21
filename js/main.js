74f7508e7c7b3ea6f1df659604f2c1a1ef55ffd4
rusSpell <48530482+rusSpell@users.noreply.github.com>
2019-10-21 23:05:57 +0300

    reload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/arrow-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="./images/arrow-right.svg" alt="">',
+        asNavFor: '.slider-dots'
+    })
+
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.header__slider',
+        focusOnSelect: true
+    });
+
+});
