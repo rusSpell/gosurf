bd8335418cff289f86d13f662f4b28ec80f8b0e4
rusSpell <48530482+rusSpell@users.noreply.github.com>
2019-10-25 19:21:01 +0300

    surf slider

--- js/main.js
+++ js/main.js
@@ -14,5 +14,10 @@ $(function () {
         asNavFor: '.header__slider',
         focusOnSelect: true
     });
-
+    $('.surf__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/arrow-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="./images/arrow-right.svg" alt="">',
+    })
 });
