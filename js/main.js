420ac313cd738d843105d2addc411b73087866f5
rusSpell <48530482+rusSpell@users.noreply.github.com>
2019-11-16 20:43:35 +0300

    section sleep

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ $(function () {
         focusOnSelect: true
     });
 
-    $('.travel__slider').slick({
+    $('.holder__slider').slick({
         infinite: true,
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="./images/arrow-left.svg" alt="">',
